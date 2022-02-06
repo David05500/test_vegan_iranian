688bfcac07e3e6244bc05a617e97586d9a309758
David <sharvashidze.david@gmail.com>
2022-02-06 15:06:35 +0000

    updated index images to Image component

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -3,74 +3,77 @@ import Link from 'next/link';
 import _ from 'lodash';
 import { AppDataContext } from '../../components/AppDataContext';
 import Meta from '../../components/shared/SeoMeta.js';
-import { Waypoint } from 'react-waypoint';
 import contentfulClient from '../../lib/contentful';
-
+import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
 const Index = ({ recipes }) => {
     const { isEnglish, paginatedRecipes, initialRecipes, setInitialRecipes, next } = useContext(AppDataContext);
+    const { ref, inView } = useInView({ threshold: 0 });
+
     useEffect(() => {
-        if (recipes) {
-            setInitialRecipes(recipes)
-        }
+        if (recipes) setInitialRecipes(recipes)
     }, [recipes])
 
+    useEffect(() => {
+        if (inView) next()
+    }, [inView])
+
+    if (!paginatedRecipes || _.isEmpty(paginatedRecipes)) return <div>Loading</div>
     return (
         <div>
             <Meta
                 title='Iranian Vegan | Recipes'
                 description='A superb collection of fine iranian vegan recipes and history behind each recipe.'
             />
-            {paginatedRecipes != null ?
-                (<div className='m-auto text-2xl bg-gray-primary'>
-                    <div className='max-width-735 px-4 lg:px-0 mx-auto lg:flex lg:flex-wrap mt-10'>
-                        {!_.isEmpty(paginatedRecipes) ?
-                            _.map(paginatedRecipes, blog => {
-                                if (blog != undefined) {
-                                    if (!isEnglish && blog.farsiTitle == null) {
-                                        if (blog.farsiTitle != null) {
-                                            return (
-                                                <div key={blog.slug} className='lg:w-1/3 mb-8'>
-                                                    <Link href='/recipes/[slug]' as={`/recipes/${blog.slug}/`}>
-                                                        <div className='card'>
-                                                            <div className='m-auto mb-4 relative pointer max-w-280px max-h-284px min-h-284px min-w-228px lg:max-w-228px   pointer hover:opacity-60 transform ease-in duration-100 '
-                                                                style={{ backgroundSize: '50%', backgroundImage: `url(${blog.smallBlogPostImage.fields.file.url})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }}>
-                                                            </div>
-                                                            <div >
-                                                                <h2 className={`text-xs text-center px-4 ${isEnglish ? 'text-xs' : 'text-base'}`}>{isEnglish ? blog.title : blog.farsiTitle}</h2>
-                                                            </div>
-                                                        </div>
-                                                    </Link>
-                                                </div>
-                                            )
-                                        }
-                                    } else {
-                                        return (
-                                            <div key={blog.slug} className='lg:w-1/3 mb-8'>
-                                                <Link href='/recipes/[slug]' as={`/recipes/${blog.slug}/`}>
-                                                    <div className='card'>
-                                                        <div className='m-auto mb-4 relative pointer max-w-280px max-h-284px min-h-284px min-w-228px lg:max-w-228px   pointer hover:opacity-60 transform ease-in duration-100 '
-                                                            style={{ backgroundSize: '50%', backgroundImage: `url(${blog.smallBlogPostImage.fields.file.url})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }}>
-                                                        </div>
-                                                        <div >
-                                                            <h2 className={`text-xs text-center px-4 transform ease-in duration-100 ${isEnglish ? 'text-xs' : 'text-base'}`}>{isEnglish ? blog.title : blog.farsiTitle}</h2>
-                                                        </div>
+            <div className='m-auto text-2xl bg-gray-primary'>
+                <div className='max-width-735 px-4 lg:px-0 mx-auto lg:flex lg:flex-wrap mt-10'>
+                    {_.map(paginatedRecipes, blog => {
+                        if (blog != undefined) {
+                            if (!isEnglish && blog.farsiTitle == null) {
+                                if (blog.farsiTitle != null) {
+                                    return (
+                                        <div key={blog.slug} className='lg:w-1/3 mb-8'>
+                                            <Link href='/recipes/[slug]' as={`/recipes/${blog.slug}/`}>
+                                                <div className='card'>
+                                                    <div className='m-auto mb-4 relative pointer max-w-280px max-h-284px min-h-284px min-w-228px lg:max-w-228px   pointer hover:opacity-60 transform ease-in duration-100 '
+                                                        style={{ backgroundSize: '50%', backgroundImage: `url(${blog.smallBlogPostImage.fields.file.url})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }}>
                                                     </div>
-                                                </Link>
-                                            </div>
-                                        )
-                                    }
+                                                    <div >
+                                                        <h2 className={`text-xs text-center px-4 ${isEnglish ? 'text-xs' : 'text-base'}`}>{isEnglish ? blog.title : blog.farsiTitle}</h2>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </div>
+                                    )
                                 }
-                            })
-                            :
-                            ''
+                            } else {
+                                return (
+                                    <div key={blog.slug} className='lg:w-1/3 p-2'>
+                                        <Link href='/recipes/[slug]' as={`/recipes/${blog.slug}/`}>
+                                            <a>
+                                                <Image
+                                                    height="345"
+                                                    width="270"
+                                                    objectFit='cover'
+                                                    alt="Jump to recipe icon"
+                                                    src={`https://${blog.smallBlogPostImage.fields.file.url}`}
+                                                    className='m-auto mb-4 relative pointer hover:opacity-60 transform ease-in duration-100 '
+                                                    prirotiy="true"
+                                                />
+                                                <h2 className={`text-xs text-center px-4 transform ease-in duration-100 ${isEnglish ? 'text-xs' : 'text-base'}`}>{isEnglish ? blog.title : blog.farsiTitle}</h2>
+                                            </a>
+                                        </Link>
+                                    </div>
+                                )
+                            }
                         }
-                    </div>
-                    <Waypoint onEnter={() => next()} />
+                    })}
                 </div>
-                )
-                :
-                (<div>Loading</div>)
-            }
+
+            </div>
+            <div ref={ref}>
+                <h1 className='invisible'>loading</h1>
+            </div>
         </div>
     )
 };
