6513ce5179cad7f4276071c8b99684a2d5523418
David <sharvashidze.david@gmail.com>
2022-09-07 21:16:44 +0100

    change of stripe account

--- pages/iranian-vegan-restaurant.js
+++ pages/iranian-vegan-restaurant.js
@@ -253,7 +253,7 @@ const IranianVeganRestaurant = () => {
                   onClick={() => {
                     checkout({
                       lineItems:[{
-                          price: "price_1LfUWwDwkFWRAyDPaN1QNkVQ",
+                          price: "price_1LfTlvDwkFWRAyDP8GxmFwSV",
                           quantity: publicLaunchQuantity
                       }
                     ]
@@ -268,7 +268,7 @@ const IranianVeganRestaurant = () => {
               onClick={() => {
                 checkout({
                   lineItems:[{
-                      price: "price_1LfUWwDwkFWRAyDPaN1QNkVQ",
+                      price: "price_1LfTlvDwkFWRAyDP8GxmFwSV",
                       quantity: privateLaunchQuantity
                   }
                 ]
